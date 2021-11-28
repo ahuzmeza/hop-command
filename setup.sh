0c19275df0eeb0b5aa6b4ee2312224a2b48d09fa
Alin <alinhuzmezan@yahoo.com>
2021-11-28 20:56:56 +0200

    v1 pre-release

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ echo "... Finished."
 
 # Edits shell_hop_source file to include current path as export
 echo -n "->] Exporting 'hop.py' to \$PAThH...|"
-awk '{gsub("/----?where?----", '$PWD', $0); print}' data.txt hop_shell_source
 $ { rm data.txt && awk '{gsub("----?where?----", $PWD, $0); print}' > hop_shell_source; } < hop_shell_source
 
 # while read a; do
