2b787bb196fc3a918e2adb614256075a5970c9b0
Alin <alinhuzmezan@yahoo.com>
2024-01-22 13:43:37 +0200

    improved code readability and generalized setup process for any shell configuration type.

--- hop.py
+++ hop.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
+
 import os
 import re
 import sys
+
 from os import getcwd
 from yaml import dump, safe_load
 from yaml.error import YAMLError
@@ -34,46 +36,46 @@ def main():
     if (len_args > 1 and len_args < 4):
         list_hops = read_hops()
         str_action = sys.argv[1].lower()
-        if (str_action == 'back'):
-            if (len_args > 2):
+        if str_action == 'back':
+            if len_args > 2:
                 return handle_error('invalid_args')
-            exit( hop_back(list_hops))
-        if (str_action == 'get'):
-            if (len_args < 3):
+            exit(hop_back(list_hops))
+        if str_action == 'get':
+            if len_args < 3:
                 return handle_error('invalid_args')
             return  hop_get(sys.argv[2], list_hops)
-        if (str_action == 'help'):
+        if str_action == 'help':
             if (len_args > 2):
                 return handle_error('invalid_args')
             return hop_help()
-        if (str_action == 'ls'):
+        if str_action == 'ls':
             if (len_args > 2):
                 return handle_error('invalid_args')
             return hop_ls(list_hops)
-        if (str_action == 'set'):
+        if str_action == 'set':
             if (len_args < 3):
                 return handle_error('invalid_args')
             return hop_set(list_hops, hop_name=sys.argv[2])
-        if (str_action == 'remove'):
-            if (len_args < 3):
+        if str_action == 'remove':
+            if len_args < 3:
                 handle_error('invalid_args')
             return hop_remove(list_hops, hop_name=sys.argv[2])
-        if (str_action == 'reset'):
-            if (len_args > 2):
+        if str_action == 'reset':
+            if len_args > 2:
                 return handle_error('invalid_args')
             return hop_reset(list_hops)
-        if (str_action == 'prune'):
-            if (len_args > 2):
+        if str_action == 'prune':
+            if len_args > 2:
                 return handle_error('invalid_args')
             return hop_prune(list_hops)
         if (str_action == 'rm' or str_action == 'delete'):
             return handle_error("use_remove_instead")
         # if str_action is not one of the above, it is considered a hop name
-        if (len(sys.argv) > 2):
+        if len(sys.argv) > 2:
             return handle_error('invalid_args')
         exit(hop_to(str_action))
     else:
-        return (handle_error('invalid_args'))
+        return handle_error('invalid_args')
 
 
 # reads hops from HOPS_FILE
@@ -99,33 +101,33 @@ def write_hops(list_hops):
         
 def hop_back(list_hops):
     # 1) check if no hop is active
-    if (list_hops['history_prev_hop'] == None):
+    if list_hops['history_prev_hop'] == None:
         handle_error('no_hop_active')
         return 0
     # 2) if given hop_name's path is an existing directory
-    if (not Path(list_hops['history_prev_hop']).is_dir()):
+    if not Path(list_hops['history_prev_hop']).is_dir():
         handle_error('dir_does_not_exist')
         return 0
     # if 1) and 2) are met, proceed to hop back at 'history_prev_hop'
-    return (list_hops['history_prev_hop'])
+    return list_hops['history_prev_hop']
 
  
 def hop_to(hop_name):
     list_hops = read_hops()
     # 1) finds if hop exists
-    if (hop_name not in list_hops):
+    if hop_name not in list_hops:
         handle_error('hop_not_found')
         return 0
     # 2) if already hopped 
-    if (list_hops[hop_name] == getcwd()):
+    if list_hops[hop_name] == getcwd():
         handle_error('already_here')
         return 0
     # 3) if given hop_name's path is an existing directory
-    if (not Path(list_hops[hop_name]).is_dir()):
+    if not Path(list_hops[hop_name]).is_dir():
         handle_error('dir_does_not_exist')
         return 0
         
-    # if 1), 2), and 3) are met, return hop_name's path
+    # if 1), 2), and 3) are passed, return hop_name's path
     hop_path = list_hops[hop_name]
     # saves path of where we're hopping from
     list_hops['history_prev_hop'] = getcwd()
@@ -137,10 +139,10 @@ def hop_ls(list_hops):
     rows, columns = os.popen('stty size', 'r').read().split()
     window_width = int(columns)
     separator_string_val = "-" * window_width
-    print( separator_string_val)
+    print(separator_string_val)
 
     # if no hops exist, except 'history_prev_hop'
-    if ( len(list_hops) == 1):
+    if len(list_hops) == 1:
         print("\t [no 'hops' to give]")
     else:    
         # 1) get previous hop path
@@ -149,7 +151,7 @@ def hop_ls(list_hops):
         # delete placeholder 'history_prev_hop' so it dosent show up
         del list_hops['history_prev_hop']
         for key in list_hops:
-            if (list_hops[key] == getcwd()):
+            if list_hops[key] == getcwd():
                 hop_name_color = Colors.GREEN+Colors.BOLD
                 hop_path_color = Colors.GREEN+Colors.BOLD
                 separator_color = Colors.GOLD+Colors.BOLD
@@ -171,13 +173,14 @@ def hop_ls(list_hops):
                     + format_path(key, list_hops[key], window_width)
                     + Colors.ENDC)                                      
         print(" ["+Colors.GREEN+f"{len(list_hops)}"+Colors.ENDC+"] hops")
+
         # 3) print last hopped from path
-        if (previous_path != "-" and previous_path != None):
+        if previous_path != "-" and previous_path != None:
             print("\n"+Colors.BGGRAY+"Last Hopped From:"+Colors.ENDC+f"\n {previous_path}")
         else:
             print("\n"+Colors.RED+"Not hopped yet"+Colors.ENDC)
 
-    print( separator_string_val)
+    print(separator_string_val)
     return 0
 
 def hop_exists_at_path(path):
@@ -192,15 +195,15 @@ def hop_get(hop_name, list_hops):
 def hop_set(list_hops, hop_name):
     # 1) checks if hop name is in restricted list
     list_restricted = ["set", "rm", "ls", "back", "help", 'history_prev_hop']
-    if (hop_name in list_restricted):
+    if hop_name in list_restricted:
         handle_error('hop_restricted_name')
         return 0
     # 2) checks if name exists already
-    if (hop_name in list_hops):
+    if hop_name in list_hops:
         handle_error('hop_already_exists')
         return 0
     # if 1) and 2) are met, set hop
-    print( f"Setting hop '{hop_name}' ...")
+    print(f"Setting hop '{hop_name}' ...")
     # create new hop entry in list_hops like: "hop_name": "_current_path_"
     list_hops.update( {hop_name: getcwd()} )
     return write_hops(list_hops)
@@ -208,7 +211,7 @@ def hop_set(list_hops, hop_name):
 
 def hop_remove(list_hops, hop_name):
     # checks if hop name exists and is not restricted
-    if (hop_name in list_hops and hop_name != 'history_prev_hop'):
+    if hop_name in list_hops and hop_name != 'history_prev_hop':
         print( f"Deleting hop '{hop_name}' ...")
         del list_hops[hop_name]
         return write_hops(list_hops)
@@ -225,10 +228,10 @@ def hop_prune(list_hops):
     # mark hops with not directories at path
     to_be_removed_hops = []
     for key in list_hops:
-        if (not Path(list_hops[key]).is_dir()):
+        if not Path(list_hops[key]).is_dir():
             to_be_removed_hops.append(key)
     # if no hops to be removed
-    if (len(to_be_removed_hops) == 0):
+    if len(to_be_removed_hops) == 0:
         handle_error('no_hops_to_remove')
         return 0
     # use hop remove to remove hops
@@ -246,12 +249,12 @@ def format_path(hop_name, hop_path, window_width):
 
 
     shortended = False
-    if (len_path > max_index):
+    if len_path > max_index:
         cut = len_path - max_index
         hop_path = hop_path[cut:] # removees "./" from path
         shortended = True 
         
-    if (shortended):
+    if shortended:
         index = hop_path.find("/")        
         hop_path = "(..)" + hop_path[index:]
     return hop_path            
@@ -271,7 +274,7 @@ def print_help():
         +"\n\thop "+Colors.GOLD+"reset"+Colors.ENDC+"\t  - removees 'last hopped from' path."
         +"\n"
     )
-    print( separator_string_val)
+    print(separator_string_val)
 
 
 def handle_error(control_value):
@@ -291,16 +294,16 @@ def handle_error(control_value):
     
     print("[hop] Error:", end=" ")
     print(list_error[control_value])
-    if (control_value == "invalid_args"):
+    if control_value == "invalid_args":
         print_help()
-    if (control_value == "hop_restricted_name"):
+    if control_value == "hop_restricted_name":
         print(Colors.RED+"Restricted names: "+Colors.ENDC+"'set', 'rm', 'ls', 'back', 'help'")
     return 1 # error was found since function was called
 
 def __init__():
-    if (not DATA_DIR.is_dir()):
+    if not DATA_DIR.is_dir():
         DATA_DIR.mkdir()
-    if (not HOPS_FILE.is_file()):
+    if not HOPS_FILE.is_file():
         with open( HOPS_FILE, "w") as stream:
             stream.write("history_prev_hop: ")  
 
--- setup.sh
+++ setup.sh
@@ -1,44 +1,38 @@
-#! /bin/bash
-
+#!/bin/sh
 
 # installs pyyaml with pip
 echo "->] Installing pyyaml..."
 pip3 install pyyaml
-echo "... Finished."
-
+echo "... Finished.\n"
 
 # Edits shell_hop_source file to include current path as export
-echo -n "->] Creating hop_shell_source...|"
+echo "->] Creating hop_shell_source...|"
 echo "# HOP COMMAND ---------------------------------------------------" > hop_shell_source
 echo "export PATH=\$PATH:$PWD" >> hop_shell_source
 cat hop_shell_source_tmp >> hop_shell_source
-echo "... Finished"
+echo "... Finished.\n"
 
+# changes hop.py permission +x
+echo "->] Setting execute permission on 'hop.py'...|"
+chmod +x hop.py
+echo "... Finished.\n"
 
 # Append sourcing file to ~/.bashrc
-echo -n "->] Appending source to '~/.bashrc'...|"
-STRING="/hop_shell_source"
-REZ_STRING=$(grep $STRING ~/.bashrc)
-echo $REZ_STRING
+echo "->] Source hop_command"
 if [ -z "$REZ_STRING" ]; then
-        echo "# Hop command --------------------------------------------------
-        source $PWD/hop_shell_source" >> ~/.bashrc
-        echo "# --------------------------------------------------------------" >> ~/.bashrc
-        echo "... Finished"
+        # Instructions for adding source command to shell configuration files
+        echo "------------------------------------------------"
+        echo "\033[1;36mTo finalize the setup, please add the following line to your shell's configuration file,"
+        echo "\033[1;36mBased on your shell and OS."
+        echo "\033[1;36mAdd this line:"
+        echo "\033[1;33m------------------------------------------------\033[0m"
+        echo "\033[0;32msource $PWD/hop_shell_source\033[0m"
+        echo "\033[1;33m------------------------------------------------\033[0m"
 else
         echo "... Skipped. Already Sourced"
 
 fi
 
-
-# changes hop.py permission +x
-echo -n "->] Setting execute permission on 'hop.py'...|"
-chmod +x hop.py
-echo "... Finished"
-
-
 # restarts terminal
-echo -n "->] Atempting to restart terminal by sourceing '~/.bashrc'...|"
-source ~/.bashrc
-echo "... Finished"
+echo "\033[1;33m Remember to restart your shell session.\033[0m\n"
 
